829ab9e89b5ac2d7f20f9460b98b8946365342a0
Shruti Chandak <99483160+shruti-2412@users.noreply.github.com>
2023-10-19 23:28:04 +0530

    Initial commit

---
+++ app/src/main/java/com/shruti/lofo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.shruti.lofo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword("abcd@gmail.com", "123");
+    }
+}
