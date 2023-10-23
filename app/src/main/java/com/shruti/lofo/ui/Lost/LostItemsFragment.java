b4c9109d5a9e6902209f0a07ae7cd6d9c6f00c1a
Isha Bule <ishabule753@gmail.com>
2023-10-24 00:59:45 +0530

    image upload feature

--- app/src/main/java/com/shruti/lofo/ui/Lost/LostItemsFragment.java
+++ app/src/main/java/com/shruti/lofo/ui/Lost/LostItemsFragment.java
@@ -1,27 +1,36 @@
 package com.shruti.lofo.ui.Lost;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioGroup;
-
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-
 import com.shruti.lofo.R;
-
 import java.util.Calendar;
 
 public class LostItemsFragment extends DialogFragment {
     private Button datePickerButton;
     private Button timePickerButton;
-    private RadioGroup radioGroup;
+    private Button uploadImageButton;
+    private Spinner categorySpinner;
+    ImageView image;
+    private EditText otherCategoryEditText;
+    final int REQ_CODE=1000;
     private int mYear, mMonth, mDay, mHour, mMinute;
 
     @NonNull
@@ -40,17 +49,75 @@ public class LostItemsFragment extends DialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
+        datePickerButton = view.findViewById(R.id.datePickerButton);
+        timePickerButton= view.findViewById(R.id.timePickerButton);
+        datePickerButton.setOnClickListener(v -> showDatePicker());
+        timePickerButton.setOnClickListener(v -> showTimePicker());
+        uploadImageButton = view.findViewById(R.id.uploadImageButton); // Initialize the button
+        uploadImageButton.setOnClickListener(v -> {
+            // Handle the upload image button click here
+        });
+
         Button submitButton = view.findViewById(R.id.submit_button);
         submitButton.setOnClickListener(v -> {
             // Handle the button click here
             // You can retrieve the data from the EditText views using their IDs
             dismiss(); // Dismiss the dialog
         });
-        datePickerButton = view.findViewById(R.id.datePickerButton);
-        timePickerButton= view.findViewById(R.id.timePickerButton);
-        datePickerButton.setOnClickListener(v -> showDatePicker());
-        timePickerButton.setOnClickListener(v -> showTimePicker());
+
+
+        categorySpinner = view.findViewById(R.id.categorySpinner);
+        otherCategoryEditText = view.findViewById(R.id.otherCategoryEditText);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(adapter);
+
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == parent.getCount() - 1) {
+                    otherCategoryEditText.setVisibility(View.VISIBLE);
+                } else {
+                    otherCategoryEditText.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle the case when nothing is selected
+            }
+        });
+
+        Button upload = view.findViewById(R.id.uploadImageButton);
+         image = view.findViewById(R.id.image);
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,REQ_CODE);
+            }
+        });
+
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQ_CODE){
+                // for gallery
+                image.setImageURI(data.getData());
+            }
+        }
+    }
+
     private void showDatePicker() {
         final Calendar c = Calendar.getInstance();
         mYear = c.get(Calendar.YEAR);
@@ -89,7 +156,18 @@ public class LostItemsFragment extends DialogFragment {
     }
 
     private void updateTimeButton() {
-        String time = mHour + ":" + mMinute;
+        String AM_PM;
+        if (mHour < 12) {
+            AM_PM = "AM";
+        } else {
+            AM_PM = "PM";
+        }
+        int hour = mHour % 12;
+        if (hour == 0) {
+            hour = 12;
+        }
+        String time = hour + ":" + mMinute + " " + AM_PM;
         timePickerButton.setText(time);
     }
+
 }
