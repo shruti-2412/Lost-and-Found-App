1a8fdcb4cfbb83a31cc5bb9293b6be64d3191508
Shruti Chandak <99483160+shruti-2412@users.noreply.github.com>
2023-10-28 23:41:47 +0530

    Profile page UI

---
+++ app/src/main/java/com/shruti/lofo/MyProfile.java
@@ -0,0 +1,15 @@
+package com.shruti.lofo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
