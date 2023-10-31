1f20af8af4e52b9df25a98545ac0a3de059cd45e
SIMRAN DESAI <desaisimran87@gmail.com>
2023-10-31 18:59:05 +0530

    hi

--- app/src/main/java/com/shruti/lofo/ui/Lost/LostDetails.java
+++ app/src/main/java/com/shruti/lofo/ui/Lost/LostDetails.java
@@ -10,12 +10,15 @@ import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.shruti.lofo.R;
-
+import com.shruti.lofo.Utility;
 
 
 public class LostDetails extends AppCompatActivity {
@@ -23,7 +26,7 @@ public class LostDetails extends AppCompatActivity {
     private ImageView img;
     private TextView title, address, mail, description, ownerName, phnum, dateLost, time, category;
     private Button call, sms, backBtn;
-    private FirebaseFirestore db; // Initialize Firestore
+    private FirebaseFirestore db; // Initialize Fire Store
 
 
     @Override
@@ -31,7 +34,7 @@ public class LostDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lost_details);
 
-        // Initialize Firestore
+        // Initialize Fire Store
         db = FirebaseFirestore.getInstance();
 
         img = findViewById(R.id.img);
@@ -48,50 +51,60 @@ public class LostDetails extends AppCompatActivity {
         sms = findViewById(R.id.sms);
         backBtn = findViewById(R.id.backBtn);
 
-        // Get the item ID from the Intent
-        String itemId = getIntent().getStringExtra("itemId");
+        String itemName = getIntent().getStringExtra("itemId"); // Assuming you pass the itemName as an extra
 
-        // Fetch item details from Firestore based on the itemId
-        DocumentReference itemRef = db.collection("lostItems").document(itemId);
-        itemRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        // Perform a query to find the document with the specific itemName
+        Query query = db.collection("lostItems").whereEqualTo("itemName", itemName);
+
+        query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) {
-                    // Get the data from the document
-                    String imageUrl = documentSnapshot.getString("imageURI");
-                    String itemTitle = documentSnapshot.getString("itemName");
-                    String itemAddress = documentSnapshot.getString("location");
-                    String itemMail = documentSnapshot.getString("email");
-                    String itemDateLost = documentSnapshot.getString("dateLost");
-                    String itemTime = documentSnapshot.getString("timeLost");
-                    String itemDescription = documentSnapshot.getString("description");
-                    String itemOwnerName = documentSnapshot.getString("ownerName");
-                    String itemCategory = documentSnapshot.getString("category");
-                    String itemPhNum = documentSnapshot.getString("phnum");
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (!queryDocumentSnapshots.isEmpty()) {
+                    // Get the document reference for the first (or only) document with the matching itemName
+                    DocumentReference itemRef = queryDocumentSnapshots.getDocuments().get(0).getReference();
 
+                    // Now you can use this itemRef to fetch the document and display its details
+                    itemRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                String imageUrl = documentSnapshot.getString("imageURI");
+                                String itemTitle = documentSnapshot.getString("itemName");
+                                String itemAddress = documentSnapshot.getString("location");
+                                String itemMail = documentSnapshot.getString("email");
+                                String itemDescription = documentSnapshot.getString("description");
+                                String itemOwnerName = documentSnapshot.getString("ownerName");
+                                String itemPhNum = documentSnapshot.getString("phone");
+                                String itemDateLost = documentSnapshot.getString("dateLost");
 
-                    if (imageUrl != null && !imageUrl.isEmpty()) {
-                        Log.d("Lost", "Image URL: " + imageUrl);
-                        Glide.with(img)
-                                .load(imageUrl)
-                                .into(img);
-                    }
+                                // Load the image using Glide
+                                if (imageUrl != null && !imageUrl.isEmpty()) {
+                                    Glide.with(LostDetails.this)
+                                            .load(imageUrl)
+                                            .placeholder(R.drawable.placeholder_image)
+                                            .error(R.drawable.baseline_image_search_24)
+                                            .into(img);
+                                }
 
-                    title.setText(itemTitle);
-                    address.setText(itemAddress);
-                    mail.setText(itemMail);
-                    dateLost.setText(itemDateLost);
-                    time.setText(itemTime);
-                    category.setText(itemCategory);
-                    description.setText(itemDescription);
-                    ownerName.setText(itemOwnerName);
-                    phnum.setText(itemPhNum);
-                }
-                else {
-                    System.out.println("Data not found!!");
+                                // Set data to TextViews
+                                title.setText(itemTitle);
+                                address.setText(itemAddress);
+                                mail.setText(itemMail);
+                                description.setText(itemDescription);
+                                ownerName.setText(itemOwnerName);
+                                phnum.setText(itemPhNum);
+                                dateLost.setText(itemDateLost);
+                            } else {
+                                Toast.makeText(LostDetails.this, "Data not found!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(LostDetails.this, "Document not found!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
 
         // Set click listeners for call and SMS buttons
         call.setOnClickListener(new View.OnClickListener() {
