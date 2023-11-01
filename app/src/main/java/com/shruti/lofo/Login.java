1e9b886285d0cac3b26cc319db3b1202d559fb89
Shruti Chandak <99483160+shruti-2412@users.noreply.github.com>
2023-11-01 21:25:35 +0530

    Profile page

--- app/src/main/java/com/shruti/lofo/Login.java
+++ app/src/main/java/com/shruti/lofo/Login.java
@@ -1,6 +1,5 @@
 package com.shruti.lofo;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,13 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
     EditText loginEmail, loginPassword;
@@ -41,7 +33,7 @@ public class Login extends AppCompatActivity {
             // User is already logged in, navigate to the main activity
             Intent intent = new Intent(this, BindingNavigation.class);
             startActivity(intent);
-            finish(); // Close the current activity to prevent going back to the main activity without login
+            finish();
         }
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,7 +93,6 @@ public class Login extends AppCompatActivity {
                         Intent intent = new Intent(Login.this, BindingNavigation.class);
                         startActivity(intent);
                     } else {
-                        // If authentication fails, display a message to the user.
                         Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                     }
                 });
--- app/src/main/java/com/shruti/lofo/ui/MyProfile/MyProfileFragment.java
+++ app/src/main/java/com/shruti/lofo/ui/MyProfile/MyProfileFragment.java
@@ -1,57 +1,76 @@
 package com.shruti.lofo.ui.MyProfile;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.shruti.lofo.R;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MyProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyProfileFragment() {
-        // Required empty public constructor
-    }
-
-    public static MyProfileFragment newInstance(String param1, String param2) {
-        MyProfileFragment fragment = new MyProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView profileName, profileEmail, profilePhone, titleName;
+    Button editProfile;
+    FirebaseFirestore database;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_my_profile, container, false);
+        profileName = root.findViewById(R.id.profileName);
+        profileEmail = root.findViewById(R.id.profileEmail);
+        profilePhone = root.findViewById(R.id.profilephone);
+        titleName = root.findViewById(R.id.titlename);
+        editProfile = root.findViewById(R.id.edit_button);
+        database = FirebaseFirestore.getInstance();
+
+        fetchUserData();
+        return root;
+    }
+    private void fetchUserData() {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        String userEmail = null;
+        if (currentUser != null) {
+            userEmail = currentUser.getEmail();
+        } else {
+            Toast.makeText(getContext(), "No user is logged in", Toast.LENGTH_SHORT).show();
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+
+        database.collection("users").document(userEmail).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            String nameFromDB = documentSnapshot.getString("name");
+                            String emailFromDB = documentSnapshot.getString("email");
+                            String phoneFromDB = documentSnapshot.getString("phone");
+
+                            titleName.setText(nameFromDB);
+                            profileName.setText(nameFromDB);
+                            profileEmail.setText(emailFromDB);
+                            profilePhone.setText(phoneFromDB);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
     }
 }
+
+
